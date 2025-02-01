d91d40c55d780e70c321bd9becb23b9c6f17a762
Isaiah Iruoha <76797894+IsaiahIruoha@users.noreply.github.com>
2025-02-01 15:28:43 -0500

    Create 2_to_1_mux.v

---
+++ 2_to_1_mux.v
@@ -0,0 +1,11 @@
+module 2_to_1_mux (input wire [31:0] in1, input wire [31:0] in2, input wire signal, output reg [31:0] out);
+
+  always @(*)begin 
+    if (signal) begin
+      out[31:0] <= in2[31:0];
+    end
+    else begin
+      out[31:0] <= in1[31:0]; 
+    end
+  end
+endmodule 
