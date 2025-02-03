47ea213ce18528eaae62b34b47a9dacec9759aa3
Isaiah Iruoha <76797894+IsaiahIruoha@users.noreply.github.com>
2025-02-03 14:24:01 -0500

    Update negate_32bit.v

--- negate_32bit.v
+++ negate_32bit.v
@@ -13,9 +13,10 @@ module negate_32bit(
     );
 
     // Step 2: Add 1 to get two’s complement
-    add_32_bit add_op (
+    add_32bit add_op (
         .Ra(temp), 
         .Rb(32'd1), 
+        .cin({1'd0}), 
         .sum(Rz), 
         .cout(cout)  // Carry-out is not used
     );
