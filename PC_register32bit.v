3dd2b55540805586ef8d09975bc5a5f41c6471a2
Isaiah Iruoha <76797894+IsaiahIruoha@users.noreply.github.com>
2025-02-06 15:09:31 -0500

    Update PC_register32bit.v

--- PC_register32bit.v
+++ PC_register32bit.v
@@ -3,8 +3,7 @@ module PC_register_32bit(
     input  wire        clear,
     input  wire        enable,
     input  wire        IncPC,
-    // If IncPC is 0 but enable is 1, we load this external value into the PC
-    input  wire [31:0] external_val,
+    input  wire [31:0] external_val,  // If IncPC is 0 but enable is 1, we load this external value 
     output wire [31:0] PC_out
 );
     wire [31:0] currentPC;   
@@ -16,7 +15,7 @@ module PC_register_32bit(
         .DATA_WIDTH_IN(32),
         .DATA_WIDTH_OUT(32),
         .INIT(32'h00000000)       // Initial PC value
-    ) u_pc_reg (
+    ) pc_reg (
         .clear(clear),
         .clock(clock),
         .enable(enable),
@@ -24,8 +23,7 @@ module PC_register_32bit(
         .BusMuxIn(currentPC)      // The output from the register
     );
 
-    // Instantiate your 32-bit adder to compute currentPC + 1
-    add_32bit pc_adder (
+    add_32bit pc_adder ( // Instantiate adder to increment PC by 1
         .Ra(currentPC),
         .Rb(32'd1),
         .cin(1'b0),
@@ -33,15 +31,11 @@ module PC_register_32bit(
         .cout(adder_cout)
     );
 
-    // Choose between adder_sum or external_val
-    always @(*) begin
+    always @(*) begin // Choose between adder_sum or external_val
         if (IncPC)
             nextPC_in = adder_sum;      // currentPC + 1
         else
             nextPC_in = external_val;   // some external load value
     end
-
-    // The register output is our PC
-    assign PC_out = currentPC;
-
+    assign PC_out = currentPC; / The register output is our PC
 endmodule
