3c6d81ba299e4bd7a4244089439331725cccaa05
Isaiah Iruoha <76797894+IsaiahIruoha@users.noreply.github.com>
2025-02-06 15:18:08 -0500

    Update and_32bit.v

--- and_32bit.v
+++ and_32bit.v
@@ -3,12 +3,10 @@ module and_32bit(
     input wire [31:0] Rb,
     output wire [31:0] Rz
 );
-
     genvar i;
     generate
         for (i = 0; i < 32; i = i + 1) begin : loop
             assign Rz[i] = Ra[i] & Rb[i];  // Bitwise AND operation
         end
     endgenerate
-
 endmodule
