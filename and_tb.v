d2fcb066470576d8c9645c2962588d8dc88be0c6
Isaiah Iruoha <76797894+IsaiahIruoha@users.noreply.github.com>
2025-02-06 15:19:31 -0500

    Update and_tb.v

--- and_tb.v
+++ and_tb.v
@@ -1,4 +1,5 @@
 `timescale 1ns/10ps
+
 module and_tb;
  reg PCout, Zlowout, MDRout, R3out, R7out;
  reg MARin, Zin, PCin, MDRin, IRin, Yin;
@@ -15,10 +16,10 @@ module and_tb;
  reg R2out,R1out,R0out,R6out,R5out,R4out,ZHighout,LOout,HIout,R15out,R14out,R13out,R12out,R11out,R10out,R9out,R8out,Cout,InPortout;
 
 parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
- Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
- T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
+Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
+T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
  
- reg [3:0] Present_state = Default;
+reg [3:0] Present_state = Default;
 
 datapath DUT(PCout, Zlowout, MDRout, R3out, R7out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, AND, R3in,
 R4in, R7in, Clock, R2out,R1out,R0out,R6out,R5out,R4out,ZHighout,LOout,HIout,R15out,R14out,R13out,R12out,R11out,R10out,R9out,R8out,Cout,InPortout, Mdatain, operation, encoder_input);
@@ -67,46 +68,46 @@ always @(Present_state) begin
             PCin <= 0; MDRin <= 0; IRin <= 0; Yin <= 0;
             IncPC <= 0; Read <= 0; AND <= 0;
             R3in <= 0; R4in <= 0; R7in <= 0;
-				R2out <= 0;R1out<= 0;R0out<= 0;R6out<= 0;R5out<= 0;R4out<= 0;ZHighout<= 0;LOout<= 0;HIout<= 0;R15out<= 0;
-				R14out<= 0;R13out<= 0;R12out<= 0;R11out<= 0;R10out<= 0;R9out<= 0;R8out<= 0;Cout<= 0;InPortout<= 0;
+	    R2out <= 0;R1out<= 0;R0out<= 0;R6out<= 0;R5out<= 0;R4out<= 0;ZHighout<= 0;LOout<= 0;HIout<= 0;R15out<= 0;
+	    R14out<= 0;R13out<= 0;R12out<= 0;R11out<= 0;R10out<= 0;R9out<= 0;R8out<= 0;Cout<= 0;InPortout<= 0;
             Mdatain <= 32'h00000000;
         end
 
         // Load value 0x22 into R3
         Reg_load1a: begin
-            Mdatain<= 32'h00000022;
-				MDRout <= 1;
-				Read <= 1; MDRin <= 1;				
-				#15 Read <= 0; MDRin <= 0; R3in <= 1;
+                Mdatain<= 32'h00000022;
+		MDRout <= 1;
+		Read <= 1; MDRin <= 1;				
+		#15 Read <= 0; MDRin <= 0; R3in <= 1;
         end
         Reg_load1b: begin
-				R3in = 1; 
-				#5 MDRout<= 0; R3in <= 0; 
+		R3in = 1; 
+		#5 MDRout<= 0; R3in <= 0; 
         end
 			
         // Load value 0x24 into R7
         Reg_load2a: begin
-            Mdatain <= 32'h00000024;
-				MDRout <= 1;
-				Read <= 1; MDRin <= 1;				
-				#15 Read <= 0; MDRin <= 0; R7in <= 1;
+                Mdatain <= 32'h00000024;
+		MDRout <= 1;
+		Read <= 1; MDRin <= 1;				
+		#15 Read <= 0; MDRin <= 0; R7in <= 1;
         end
         Reg_load2b: begin
-				R7in = 1; 
-				#5 MDRout <= 0; R7in <= 0;
+		R7in = 1; 
+		#5 MDRout <= 0; R7in <= 0;
         end
 
         // Load value 0x28 into R4
         Reg_load3a: begin
-            Mdatain <= 32'h00000028;
-				MDRout <= 1;
-				Read <= 1; MDRin <= 1;				
-				#15 Read <= 0; MDRin <= 0; R4in <= 1;
+                Mdatain <= 32'h00000028;
+		MDRout <= 1;
+		Read <= 1; MDRin <= 1;				
+		#15 Read <= 0; MDRin <= 0; R4in <= 1;
         end
         Reg_load3b: begin
-            R4in = 1; 
-				#5 MDRout<= 0; R4in <= 0;
-				#5 IncPC <= 1;
+            	R4in = 1; 
+		#5 MDRout<= 0; R4in <= 0;
+		#5 IncPC <= 1;
         end
 
         // Start AND operation (AND R4, R3, R7)
@@ -128,20 +129,19 @@ always @(Present_state) begin
 
         T3: begin
             R3out <= 1; 
-				#5 Yin <= 1;
-				#10 Yin <= 0; R3out <= 0;
+	    #5 Yin <= 1;
+	    #10 Yin <= 0; R3out <= 0;
         end
 
         T4: begin
             R7out <= 1; AND <= 1; Zin <= 1; operation <= 5'b00101;
             #10 R7out <= 0; AND <= 0; Zin <= 0;
-				#5 Zlowout <= 1; R4in <= 1;
+	    #5 Zlowout <= 1; R4in <= 1;
         end
 
         T5: begin
             #10 Zlowout <= 0; R4in <= 0;
         end
     endcase
-end
-
+   end
 endmodule
