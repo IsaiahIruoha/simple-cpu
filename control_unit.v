d3a803b1c44276d834f4640cc8c685ff4bc7274a
Alex Morra <144852352+amorra1@users.noreply.github.com>
2025-03-20 12:31:33 -0400

    Phase 3 done :0

--- control_unit.v
+++ control_unit.v
@@ -1,27 +1,29 @@
 `timescale 1ns/10ps
 
 module control_unit (
-	output reg Gra, Grb, Grc, Rin, Rout, LOout, HIout, ZLowout, ZHighout, MDRout, PCout, CON_out, InPortout, // define the inputs and outputs to your Control Unit here
+	output reg Gra, Grb, Grc, Rin, Rout, LOout, HIout, ZLowout, ZHighout, MDRout, PCout, InPortout, // define the inputs and outputs to your Control Unit here
 				  BAout, Cout, OutPortin, MDRin, MARin, Yin, ZHighIn, ZLowIn, IRin, PCin, CON_in, LOin, HIin, R8in, IncPC,
 				  Read, Write, Clear, Run, 
 	output reg [4:0] operation,
+	input CON_out,
 	input [31:0] IR,
 	input Clock, Reset, Stop);
 	
-parameter reset_state= 8'b00000000, fetch0 = 8'b00000001, fetch1 = 8'b00000010, fetch2= 8'b00000011,
-			 add3 = 8'b00000100, add4= 8'b00000101, add5= 8'b00000110, sub3 = 8'b00000111, sub4 = 8'b00001000, sub5 = 8'b00001001,
-			 mul3 = 8'b00001010, mul4 = 8'b00001011, mul5 = 8'b00001100, mul6 = 8'b00001101, div3 = 8'b00001110, div4 = 8'b00001111,
-			 div5 = 8'b00010000, div6 = 8'b00010001, or3 = 8'b00010010, or4 = 8'b00010011, or5 = 8'b00010100, and3 = 8'b00010101, 
-			 and4 = 8'b00010110, and5 = 8'b00010111, shl3 = 8'b00011000, shl4 = 8'b00011001, shl5 = 8'b00011010, shr3 = 8'b00011011,
-			 shr4 = 8'b00011100, shr5 = 8'b00011101, rol3 = 8'b00011110, rol4 = 8'b00011111, rol5 = 8'b00100000, ror3 = 8'b00100001,
-			 ror4 = 8'b00100010, ror5 = 8'b00100011, neg3 = 8'b00100100, neg4 = 8'b00100101, neg5 = 8'b00100110, not3 = 8'b00100111,
-			 not4 = 8'b00101000, not5 = 8'b00101001, ld3 = 8'b00101010, ld4 = 8'b00101011, ld5 = 8'b00101100, ld6 = 8'b00101101, 
-			 ld7 = 8'b00101110, ldi3 = 8'b00101111, ldi4 = 8'b00110000, ldi5 = 8'b00110001, st3 = 8'b00110010, st4 = 8'b00110011,
-			 st5 = 8'b00110100, st6 = 8'b00110101, st7 = 8'b00110110, addi3 = 8'b00110111, addi4 = 8'b00111000, addi5 = 8'b00111001,
-			 andi3 = 8'b00111010, andi4 = 8'b00111011, andi5 = 8'b00111100, ori3 = 8'b00111101, ori4 = 8'b00111110, ori5 = 8'b00111111,
-			 br3 = 8'b01000000, br4 = 8'b01000001, br5 = 8'b01000010, br6 = 8'b01000011, br7 = 8'b11111111, jr3 = 8'b01000100, jal3 = 8'b01000101, 
-			 jal4 = 8'b01000110, mfhi3 = 8'b01000111, mflo3 = 8'b01001000, in3 = 8'b01001001, out3 = 8'b01001010, nop3 = 8'b01001011, 
-			 halt3 = 8'b01001100;
+parameter reset_state = 8'b00000000, fetch0 = 8'b00000001, fetch1 = 8'b00000010, fetch2 = 8'b00000011,
+add3 = 8'b00000100, add4 = 8'b00000101, add5 = 8'b00000110, sub3 = 8'b00000111, sub4 = 8'b00001000, sub5 = 8'b00001001,
+mul3 = 8'b00001010, mul4 = 8'b00001011, mul5 = 8'b00001100, mul6 = 8'b00001101, div3 = 8'b00001110, div4 = 8'b00001111,
+div5 = 8'b00010000, div6 = 8'b00010001, or3 = 8'b00010010, or4 = 8'b00010011, or5 = 8'b00010100, and3 = 8'b00010101,
+and4 = 8'b00010110, and5 = 8'b00010111, shl3 = 8'b00011000, shl4 = 8'b00011001, shl5 = 8'b00011010, shr3 = 8'b00011011,
+shr4 = 8'b00011100, shr5 = 8'b00011101, shra3 = 8'b00011110, shra4 = 8'b00011111, shra5 = 8'b00100000, rol3 = 8'b00100001,
+rol4 = 8'b00100010, rol5 = 8'b00100011, ror3 = 8'b00100100, ror4 = 8'b00100101, ror5 = 8'b00100110, neg3 = 8'b00100111,
+neg4 = 8'b00101000, neg5 = 8'b00101001, not3 = 8'b00101010, not4 = 8'b00101011, not5 = 8'b00101100, ld3 = 8'b00101101,
+ld4 = 8'b00101110, ld5 = 8'b00101111, ld6 = 8'b00110000, ld7 = 8'b00110001, ldi3 = 8'b00110010, ldi4 = 8'b00110011,
+ldi5 = 8'b00110100, st3 = 8'b00110101, st4 = 8'b00110110, st5 = 8'b00110111, st6 = 8'b00111000, st7 = 8'b00111001,
+addi3 = 8'b00111010, addi4 = 8'b00111011, addi5 = 8'b00111100, andi3 = 8'b00111101, andi4 = 8'b00111110, andi5 = 8'b00111111,
+ori3 = 8'b01000000, ori4 = 8'b01000001, ori5 = 8'b01000010, br3 = 8'b01000011, br4 = 8'b01000100, br5 = 8'b01000101,
+br6 = 8'b01000110, br7 = 8'b01000111, jr3 = 8'b01001000, jr4 = 8'b01001001, jal3 = 8'b01001010, jal4 = 8'b01001011, 
+mfhi3 = 8'b01001100, mflo3 = 8'b01001101, in3 = 8'b01001110, out3 = 8'b01001111, nop3 = 8'b01010000, halt3 = 8'b01010001;
+
 
 reg [7:0] present_state = reset_state; // adjust the bit pattern based on the number of states
 
@@ -41,6 +43,7 @@ always @(posedge Clock, posedge Reset) // finite state machine; if clock or rese
 					5'b10000	: present_state = mul3;
 					5'b01111	: present_state = div3;
 					5'b01001	: present_state = shr3;
+					5'b01010 : present_state = shra3;
 					5'b01011	: present_state = shl3;
 					5'b00111	: present_state = ror3;
 					5'b01000	: present_state = rol3;
@@ -105,6 +108,10 @@ always @(posedge Clock, posedge Reset) // finite state machine; if clock or rese
 		shr4 : present_state = shr5;
 		shr5 : present_state = reset_state;
 		
+		shra3 : present_state = shra4;
+		shra4 : present_state = shra5;
+		shra5 : present_state = reset_state;
+		
 		rol3 : present_state = rol4;
 		rol4 : present_state = rol5;
 		rol5 : present_state = reset_state;
@@ -146,7 +153,8 @@ always @(posedge Clock, posedge Reset) // finite state machine; if clock or rese
 		jal3 : present_state = jal4;
 		jal4 : present_state = reset_state;
 		
-		jr3 : present_state = reset_state;
+		jr3 : present_state = jr4;
+		jr4 : present_state = reset_state;
 		
 		br3 : present_state = br4;
 		br4 : present_state = br5;
@@ -172,7 +180,7 @@ always @(present_state) // do the job for each state
 		case (present_state) // assert the required signals in each state
 			reset_state: begin
 					Run <= 1; 
-					Gra <= 0; Grb <= 0; Grc <= 0; Rin <= 0; Rout <= 0; LOout <= 0; HIout <= 0; ZLowout <= 0; ZHighout <= 0; MDRout <= 0; PCout <= 1; CON_out <= 0; InPortout <= 0;
+					Gra <= 0; Grb <= 0; Grc <= 0; Rin <= 0; Rout <= 0; LOout <= 0; HIout <= 0; ZLowout <= 0; ZHighout <= 0; MDRout <= 0; PCout <= 1; InPortout <= 0;
 				   BAout <= 0; Cout <= 0; OutPortin <= 0; MDRin <= 0; MARin <= 1; Yin <= 0; ZHighIn <= 0; ZLowIn <= 0; IRin <= 0; PCin <= 0; CON_in <= 0; LOin <= 0; HIin <= 0; R8in <= 0; IncPC <= 1;
 				   Read <= 0; Write <= 0; Clear <= 0;
 				   operation <= 4'b0000;
@@ -194,14 +202,19 @@ always @(present_state) // do the job for each state
 					#10 MDRout <= 0; IRin <= 0;
 			end
 			
-			// add and subtract
-			add3, sub3: begin
+			// add and subtract, and, or, shl, shr, shra, rol, ror
+			add3, sub3, or3, and3, shl3, shr3, shra3, rol3, ror3: begin
 					Grb <= 1; Rout <= 1; Yin <= 1;
 			end
 			
-			add4, sub4: begin
+			add4: begin
 					Grb <= 0; Rout <= 0; Yin <= 0;
-					Grc <= 1; Rout <= 1; ZLowIn <= 1; 
+					Grc <= 1; Rout <= 1; operation <= 5'b00011; ZLowIn <= 1; 
+			end
+			
+			sub4: begin
+					Grb <= 0; Rout <= 0; Yin <= 0;
+					Grc <= 1; Rout <= 1; operation <= 5'b00100; ZLowIn <= 1; 
 			end
 			
 			add5, sub5: begin
@@ -209,31 +222,60 @@ always @(present_state) // do the job for each state
 					ZLowout <= 1; Gra <= 1; Rin <= 1;
 			end
 			
-			// and, or, shl, shr, rol, ror
-			or3, and3, shl3, shr3, rol3, ror3: begin	
-					Grb <= 1; Rout <= 1; Yin <= 1;
-			end
-			
-			or4, and4, shl4, shr4, rol4, ror4: begin
+			or4: begin
 					Grb <=0; Rout <= 0; Yin <= 0;
-					Grc <=1; Rout <= 1; ZHighIn <= 1; ZLowIn <= 1;
+					Grc <=1; Rout <= 1; operation <= 5'b00110; ZHighIn <= 1; ZLowIn <= 1;
 			end
 			
-			or5, and5, shl5, shr5, rol5, ror5: begin
+			and4: begin
+					Grb <=0; Rout <= 0; Yin <= 0;
+					Grc <=1; Rout <= 1; operation <= 5'b00101; ZHighIn <= 1; ZLowIn <= 1;
+			end
+			
+			shl4: begin
+					Grb <=0; Rout <= 0; Yin <= 0;
+					Grc <=1; Rout <= 1; operation <= 5'b01011; ZHighIn <= 1; ZLowIn <= 1;
+			end
+			
+			shr4: begin
+					Grb <=0; Rout <= 0; Yin <= 0;
+					Grc <=1; Rout <= 1; operation <= 5'b01001; ZHighIn <= 1; ZLowIn <= 1;
+			end
+			
+			shra4: begin
+					Grb <=0; Rout <= 0; Yin <= 0;
+					Grc <=1; Rout <= 1; operation <= 5'b01010; ZHighIn <= 1; ZLowIn <= 1;
+			end
+			
+			rol4: begin
+					Grb <=0; Rout <= 0; Yin <= 0;
+					Grc <=1; Rout <= 1; operation <= 5'b01000; ZHighIn <= 1; ZLowIn <= 1;
+			end
+			
+			ror4: begin
+					Grb <=0; Rout <= 0; Yin <= 0;
+					Grc <=1; Rout <= 1; operation <= 5'b00111; ZHighIn <= 1; ZLowIn <= 1;
+			end
+			
+			or5, and5, shl5, shr5, shra5, rol5, ror5: begin
 					Grc <= 0; Rout <= 0; ZHighIn <= 0; ZLowIn <= 0;
 					ZLowout <= 1; Gra <= 1; Rin <= 1;
-					#10 ZLowout <= 0; Gra <= 1; Rout <= 1; Rin <= 0;
 			end
 			
 			// mul and div
 			mul3, div3: begin	
-					Grb <= 1; Rout <= 1; Yin <= 1;  
+					Gra <= 1; Rout <= 1; Yin <= 1;  
 			
 			end
 			
-			mul4, div4: begin
-					Grb <= 0; Rout <= 0; Yin <= 0;
-					Grc <= 1; Rout <= 1; ZHighIn <= 1; ZLowIn <= 1;
+			mul4: begin
+					Gra <= 0; Rout <= 0; Yin <= 0;
+					Grb <= 1; Rout <= 1; operation <= 5'b10000; ZHighIn <= 1; ZLowIn <= 1;
+			end
+			
+			div4: begin
+					Gra <= 0; Rout <= 0; Yin <= 0;
+					Grb <= 1; Rout <= 1; operation <= 5'b01111; ZHighIn <= 1; ZLowIn <= 1;
 			end
 			
 			mul5, div5: begin
@@ -247,8 +289,12 @@ always @(present_state) // do the job for each state
 			end
 			
 			// not and neg
-			not3, neg3: begin	
-					Grb<=1; Rout <= 1; ZHighIn <= 1; ZLowIn <= 1;
+			not3: begin	
+					Grb<=1; Rout <= 1; operation <= 5'b10010; ZHighIn <= 1; ZLowIn <= 1;
+			end
+			
+			neg3: begin	
+					Grb<=1; Rout <= 1; operation <= 5'b10001; ZHighIn <= 1; ZLowIn <= 1;
 			end
 			
 			not4, neg4: begin
@@ -305,7 +351,7 @@ always @(present_state) // do the job for each state
 			
 			st4: begin
 					Cout <= 1; operation <= 5'b00011; ZLowIn <= 1;
-					#10 Cout <= 0; ZLowIn <= 0; ZLowout = 1; MARin = 1;
+					#15 Cout <= 0; ZLowIn <= 0; ZLowout = 1; MARin = 1;
 			end
 
 			st5: begin
@@ -325,12 +371,22 @@ always @(present_state) // do the job for each state
 			// addi, ori, andi
 			addi3, ori3, andi3: begin
 					Yin <= 1; Grb <= 1; Rout <= 1;
-					#10 Grb <= 0; Rout <= 0; Yin <= 0; Cout <= 1;
+					#15 Grb <= 0; Rout <= 0; Yin <= 0; Cout <= 1;
 			end
 			
-			addi4, ori4, andi4: begin
+			addi4: begin
+					Cout <= 1; operation <= 5'b00011; ZLowIn <= 1;
+					#15 Cout <= 0; ZLowIn <= 0; ZLowout <= 1; Gra <= 1; Rin <= 1;
+			end
+			
+			ori4: begin
+					Cout <= 1; operation <= 5'b00110; ZLowIn <= 1;
+					#15 Cout <= 0; ZLowIn <= 0; ZLowout <= 1; Gra <= 1; Rin <= 1;
+			end
+			
+			andi4: begin
 					Cout <= 1; operation <= 5'b00101; ZLowIn <= 1;
-					#10 Cout <= 0; ZLowIn <= 0; ZLowout <= 1; Gra <= 1; Rin <= 1;
+					#15 Cout <= 0; ZLowIn <= 0; ZLowout <= 1; Gra <= 1; Rin <= 1;
 			end
 			
 			addi5, ori5, andi5: begin
@@ -350,7 +406,7 @@ always @(present_state) // do the job for each state
 			br5: begin
 					Cout <= 1; operation <= 5'b00011; ZLowIn <= 1;
 					if (CON_out) begin
-						#10 ZLowout <= 1; PCin <= 1; Cout <= 0; ZLowIn <= 0;
+						#15 ZLowout <= 1; PCin <= 1; Cout <= 0; ZLowIn <= 0;
 					end
 			end
 			
@@ -360,8 +416,12 @@ always @(present_state) // do the job for each state
 			
 			// jumps
 			jr3: begin
-					Gra <= 1; Rout <= 1; PCin <= 1;
-					#15 Gra <= 0; Rout <= 0; PCin <= 0;
+					Gra <= 1; Rout <= 1;
+					#15 Gra <= 0; Rout <= 0; PCin <= 1;
+			end
+			
+			jr4: begin
+					#5 PCin <= 0;
 			end
 			
 			jal3: begin
--- testbenches/phase3_tb.v
+++ testbenches/phase3_tb.v
@@ -4,7 +4,7 @@ module phase3_tb;
 	reg clock, reset, stop;
 	wire [31:0] device_data, OutPort_data_out;
 	
-	wire [31:0] mdr_data_out, PC_data, bus_data, IR_data, mar_data, R0_data, R1_data, R2_data, R3_data, R4_data, R5_data, R6_data, R7_data, R8_data, R9_data, R10_data, Z_data, Y_data;
+	wire [31:0] mdr_data_out, PC_data, bus_data, IR_data, mar_data, R0_data, R1_data, R2_data, R3_data, R4_data, R5_data, R6_data, R7_data, R8_data, R9_data, R10_data, R11_data, R12_data, R13_data, R14_data, R15_data, HI_data, LO_data, Z_data, Y_data;
 	wire [7:0] present_state;
 	wire [4:0] operation;
 	wire [63:0] c_data;
@@ -29,6 +29,14 @@ datapath DUT(.clock(clock), .reset(reset), .stop(stop), .device_data(inport_data
  assign R8_data = DUT.R8_data_out;
  assign R9_data = DUT.R9_data_out;
  assign R10_data = DUT.R10_data_out;
+ assign R11_data = DUT.R11_data_out;
+ assign R12_data = DUT.R12_data_out;
+ assign R13_data = DUT.R13_data_out;
+ assign R14_data = DUT.R14_data_out;
+ assign R15_data = DUT.R15_data_out;
+ assign HI_data = DUT.HI_data_out;
+ assign LO_data = DUT.LO_data_out;
+ assign R10_data = DUT.R10_data_out;
  assign Z_data = DUT.ZLow_data_out;
  assign operation = DUT.operation;
  assign c_data = DUT.c_data_out;
@@ -39,9 +47,8 @@ datapath DUT(.clock(clock), .reset(reset), .stop(stop), .device_data(inport_data
  assign CONout = DUT.CON_output;
  assign Yin = DUT.Y_enable;
  assign CONin = DUT.CON_in;
-// assign D = DUT.conff_unit.flip_flop.D;
-// assign Q = DUT.conff_unit.flip_flop.Q;
-// assign CON = DUT.conff_unit.CON;
+ assign Zout = DUT.ZLowout;
+ assign PCin = DUT.PC_enable;
  
 initial
 	begin
